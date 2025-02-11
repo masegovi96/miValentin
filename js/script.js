3bcfc928781f060d4cafcdb87a9bd12ecf4edb17
masegovi <mario.segovia.gtz@outlook.com>
2025-02-10 22:21:36 -0600

    Modificación de estilos, y del index. Se agregó canción y transiciones para el si

--- js/script.js
+++ js/script.js
@@ -2,12 +2,32 @@ setTimeout(() => {
     document.getElementById('scrolling-text').style.display = 'none';
     document.getElementById('main-content').classList.remove('hidden');
     startHeartAnimation();
-}, 11000);
+}, 30000);
 
 document.getElementById('yes-btn').addEventListener('click', function() {
-    document.getElementById('funny-img').src = 'https://i.pinimg.com/736x/a8/c3/95/a8c395ab6e9f3ca01c7c2d9ffc33bb86.jpg';
-    document.getElementById('funny-img').classList.remove('hidden');
+    let funnyImg = document.getElementById('funny-img');
+    let loveText = document.getElementById('love-text');
+    
+    funnyImg.src = 'https://i.pinimg.com/736x/a8/c3/95/a8c395ab6e9f3ca01c7c2d9ffc33bb86.jpg';
+    funnyImg.classList.remove('hidden');
+
+    let mainContent = document.getElementById('main-content');
+    mainContent.style.display = 'flex';
+    mainContent.style.flexDirection = 'column';
+    mainContent.style.alignItems = 'center';
+    mainContent.style.justifyContent = 'center';
+
     document.getElementById('buttons').style.display = 'none';
+    document.getElementById('title-container').style.display = 'none';
+
+    setTimeout(() => {
+        loveText.classList.remove('hidden');
+        loveText.classList.add('animated-text');
+    }, 3000);
+
+
+    let loveSong = document.getElementById('love-song');
+    loveSong.play();
 });
 
 let noBtn = document.getElementById('no-btn');
@@ -17,6 +37,7 @@ noBtn.addEventListener('mouseover', function() {
     noBtn.style.position = 'absolute';
     noBtn.style.left = `${x}px`;
     noBtn.style.top = `${y}px`;
+    console.log(x, y);
 });
 
 function createHeart(){
@@ -28,7 +49,7 @@ function createHeart(){
     document.body.appendChild(heart);
     setTimeout(() => {
         heart.remove();
-    }, 5000);
+    }, 3000);
 }
 
 function startHeartAnimation() {
